f175fbe69d1c2d2a4b2bec2bfa6dd6f1b923200d
Anton Suslov <anton_suslov@me.com>
2022-07-31 13:34:31 +0300

    main.py now spits out all the top-level records

--- main.py
+++ main.py
@@ -3,63 +3,21 @@ import struct
 import enum
 from collections import namedtuple
 
+from oblivion_types import Flags, oblivion_parser
+
 FILENAME = sys.argv[1]
-
-RecordHeader = namedtuple("Record", ["record_type", "size", "flags", "formid", "m"])
-RecordType = namedtuple("RecordType", ["type", "format"])
-
-RECORD = "<4sLLLL"
-
-class Flags(enum.Enum):
-    ESM_RECORD = 0x00000001
-    DELETED = 0x00000020
-    CASTS_SHADOWS = 0x00000200
-    PERS_REF = 0x00000400
-    INIT_DISABLED = 0x00000800
-    IGNORED = 0x00001000
-    DIST_VIS = 0x00008000
-    OFF_LIMITS = 0x00020000
-    COMPRESSED = 0x00040000
-    CANT_WAIT = 0x00080000
-
-
-def read_struct(fmt, f, type=None):
-    size = struct.calcsize(fmt)
-    buf = f.read(size)
-    if len(buf) == 0:
-        raise ValueError("EOF")
-    elif len(buf) != size:
-        raise ValueError("OOPS")
-    tup = struct.unpack(fmt, buf)
-    if type is None:
-        return tup
-
-    return type._make(tup)
-
-
-class Header():
-    def __init__(self, nt):
-        self.tuple = nt
-        self.flags = [ flag for flat in Flags if fnt.flags & flag.value ]
-
-    def __str__():
-        return "Header {}, {} bytes, {}".format(self.tuple.record_type.decode('ASCII'), self.tuple.size, ", ".join([flag.name for flag in self.flags]))
-
-
-def read_header(f):
-    header_tuple = read_struct(RECORD, f, RecordHeader)
-    header = Header(header_tuple)
-    return header
-
 
 with open(FILENAME, 'rb') as f:
     while True:
-        header = read_struct(RECORD, f, RecordHeader)
-        rec_type, size, flags, formid, _ = read_struct(RECORD, f)
+        header_bytes = f.read(oblivion_parser.sizeof("record_header"))
+        if header_bytes == b"":
+            break
 
-        flag_list = [ flag.name for flag in Flags if flags & flag.value ]
+        header = oblivion_parser.parse_from_bytes("record_header", header_bytes)
 
-        print("Record {}, {} bytes, {}".format(rec_type.decode('ASCII'), size,
+        flag_list = [ flag.name for flag in Flags if header.flags & flag.value ]
+
+        print("Record {}, {} bytes, {}".format(header.type.decode('ASCII'), header.size,
             ", ".join(flag_list)))
 
-        f.seek(size, 1)
+        f.seek(header.size, 1)
---
+++ oblivion_types.py
@@ -0,0 +1,30 @@
+import enum
+
+import binaryparse
+
+class Flags(enum.Enum):
+    ESM_RECORD = 0x00000001
+    DELETED = 0x00000020
+    CASTS_SHADOWS = 0x00000200
+    PERS_REF = 0x00000400
+    INIT_DISABLED = 0x00000800
+    IGNORED = 0x00001000
+    DIST_VIS = 0x00008000
+    OFF_LIMITS = 0x00020000
+    COMPRESSED = 0x00040000
+    CANT_WAIT = 0x00080000
+
+
+oblivion_parser = binaryparse.BinaryParser()
+
+RecordHeader = [
+        ("record_type", "type"),
+        ("unsigned long", "size"),
+        ("unsigned long", "flags"),
+        ("unsigned long", "formid"),
+        ("unsigned long", "m"),
+]
+
+oblivion_parser.typedef("record_type", "4s")
+
+oblivion_parser.register("record_header", RecordHeader)
